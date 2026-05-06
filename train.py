a797dd4f0e33aa6f5a2e58cafb09848b825d3a1b
Allen Abraham <allenabraham106@gmail.com>
2026-05-05 21:51:12 -0600

    adding our masks

--- train.py
+++ train.py
@@ -23,5 +23,24 @@ class CropDataset:
         image_path = os.path.join(self.dataset_path, "field_images/rgb", image_name)
         image = cv2.imread(image_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        anomaly_to_yield = {
+            "double_plant": 1,
+            "drydown": 2,
+            "nutrient_deficiency": 2,
+            "planter_skip": 2,
+            "storm_damage": 2,
+            "weed_cluster": 2,
+            "water": 2,
+            "waterway": 2,
+        }
+    
+        mask = np.zeros((512, 512), dtype = np.int64)
+        for anomoly, class_id in anomaly_to_yield.items():
+            mask_name = image_name.replace(".jpg", ".png")
+            mask_path = os.path.join(self.dataset_path, "field_labels", anomoly, mask_name)
+            if os.path.exists(mask_path):
+                anomoly_mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+                mask[anomoly_mask == 255] = class_id  #creates a true or false grid, true if anomoly exists
 
-
+        image = torch.tensor(image).permute(2, 0, 1).float() / 255.0
+        mask = torch.tensor(mask).long()
